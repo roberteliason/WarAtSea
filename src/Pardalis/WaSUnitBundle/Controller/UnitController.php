47e3ed2dcb5f2b24214644e31af30563eb84d6ae
Robert Eliason <roberte@orestad-linux.se>
2014-05-20 22:37:06 +0200

    This should be in its own controller

--- src/Pardalis/WaSUnitBundle/Controller/UnitController.php
+++ src/Pardalis/WaSUnitBundle/Controller/UnitController.php
@@ -35,14 +35,14 @@ class UnitController extends Controller
 		return $this->render('PardalisWaSUnitBundle:Unit:unit.html.twig', array( 'unit' => $unit ) );
 	}
 
-	public function abilitiesAction()
-	{
-		$abilities = $this->getDoctrine()
-			->getRepository('PardalisWaSUnitBundle:Ability')
-			->findAll();		
+	// public function abilitiesAction()
+	// {
+	// 	$abilities = $this->getDoctrine()
+	// 		->getRepository('PardalisWaSUnitBundle:Ability')
+	// 		->findAll();		
 
-		return $this->render('PardalisWaSUnitBundle:Ability:index.html.twig', array( 'abilities' => $abilities ) );
-	}
+	// 	return $this->render('PardalisWaSUnitBundle:Ability:index.html.twig', array( 'abilities' => $abilities ) );
+	// }
 
 	public function unittypesAction()
 	{
